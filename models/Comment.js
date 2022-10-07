eb069175ea4457346fce06a99722ddfb921ebb3e
yxx-dev <hermitlun@gmail.com>
2022-10-07 13:19:19 -0700

    comment model added

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
     },
     content: {
       type: DataTypes.STRING,

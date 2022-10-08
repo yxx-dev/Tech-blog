043855a2d799e2403ea1ff5b18f96deabe7be855
yxx-dev <hermitlun@gmail.com>
2022-10-07 21:02:10 -0700

    dashboard and blog routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const { User, Blog, Comment } = require('../../models');
 
-router.get('/', async (req, res) => {
-    res.render('login');
-  });
+router.get('/:blogId', async (req, res) => {
+  try {
+
+    res.render('blog');
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
+const auth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
+
+  
   res.render('homepage');
+});
+
+router.get('/dashboard', auth, async (req, res) => {
+  try {
+    res.render('dashboard');
+
+  } catch(err) {
+    res.status.json(err);
+  }
+
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;

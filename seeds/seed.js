cf6dadd70dd9cc03ed7235971eed44e35b62df12
yxx-dev <hermitlun@gmail.com>
2022-10-07 20:25:22 -0700

    login/out routes

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,7 +8,10 @@ const commentSeedData = require('./commentSeedData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userSeedData);
+  const users = await User.bulkCreate(userSeedData, {
+    individualHooks: true,
+    returning: true,
+  });
   const blogs = await Blog.bulkCreate(blogSeedData);
   const comments = await Comment.bulkCreate(commentSeedData);
 

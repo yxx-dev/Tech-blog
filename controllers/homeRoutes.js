8931ce3c3b180c1afbc2de7a57c003ab31236406
yxx-dev <hermitlun@gmail.com>
2022-10-08 22:31:11 -0700

    login out ok

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
     const blogs = blogData.map((content) => content.get({ plain: true }));
     res.render('homepage', {
       blogs,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);

b3562925ff477fed97898410d5810aaa24feae47
yxx-dev <hermitlun@gmail.com>
2022-10-08 22:32:58 -0700

    fixed dashboard logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,9 @@ router.get('/', async (req, res) => {
 
 router.get('/dashboard', auth, async (req, res) => {
   try {
-    res.render('dashboard');
+    res.render('dashboard',{
+      logged_in: req.session.logged_in
+    });
 
   } catch(err) {
     res.status(404).json(err);

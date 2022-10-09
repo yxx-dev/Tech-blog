1336624a974b4df4054f399476d0dbfe474f2b76
yxx-dev <hermitlun@gmail.com>
2022-10-09 15:21:15 -0700

    display formatted date on home page

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // the helper method 'format_date' will take in a timestamp and return a string with only the date
+    format_date: (date) => {
+      return date.toLocaleDateString();
+    },
+  };
+  

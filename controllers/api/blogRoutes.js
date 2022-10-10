e3fd9ba9f8ccf708b950e75a843fb9b2643c7b2b
yxx-dev <hermitlun@gmail.com>
2022-10-09 21:32:36 -0700

    added dashboard

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,27 @@
 const router = require('express').Router();
+const auth = require('../../utils/auth');
 const { User, Blog, Comment } = require('../../models');
+
+router.get('/dashboard', auth, async (req, res) => {
+  try {
+    const userBlogsData = await Blog.findAll({
+      include: { 
+        model: User, 
+        attributes: { exclude: ['password'] }
+      },
+      where: { user_id: req.session.user_id}
+    })
+    const userBlogs = userBlogsData.map((content) => content.get({ plain: true }));
+    // res.json(userBlogs);
+    res.render('dashboard', {
+      userBlogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 
 router.get('/:blogId', async (req, res) => {
   try {
